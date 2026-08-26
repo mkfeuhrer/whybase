b103a3889e6c82d0129b54fa49920a4fa96c70a8
Mohit Khare <16002727+mkfeuhrer@users.noreply.github.com>
2026-08-26 14:11:28 +0530

    feat: scaffold module, config, root command

---
+++ internal/cli/root.go
@@ -0,0 +1,32 @@
+package cli
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	flagRoot string
+	stderr   = os.Stderr
+)
+
+func NewRoot() *cobra.Command {
+	c := &cobra.Command{
+		Use:           "whybase",
+		Short:         "Every why in your codebase, one query away",
+		SilenceUsage:  true,
+		SilenceErrors: true,
+	}
+	c.PersistentFlags().StringVar(&flagRoot, "dir", ".", "repository root")
+	return c
+}
+
+func Execute() error {
+	if err := NewRoot().Execute(); err != nil {
+		fmt.Fprintln(stderr, "error:", err)
+		return err
+	}
+	return nil
+}
